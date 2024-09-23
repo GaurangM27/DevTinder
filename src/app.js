ff819f31d786ee607942eefc22527dc85b856922
GaurangM27 <gaurangmishra2712@gmail.com>
2024-09-23 12:26:52 +0530

    play with route handlers

--- src/app.js
+++ src/app.js
@@ -2,13 +2,45 @@ const express = require("express");
 
 const app = express();
 
-app.use("/home", (req, res) => {
-  res.send("Welcome to Home Page");
+app.get("/user", (req, res) => {
+  res.send("Welcome to all the users");
 });
 
-app.use("/test", (req, res) => {
-  res.send("Testing Page");
+app.post("/user", (req, res) => {
+  res.send("Post new user");
 });
+
+app.put("/user", (req, res) => {
+  res.send("Update user");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Delete user");
+});
+
+app.use(
+  "/play",
+  [
+    (req, res, next) => {
+      console.log("1");
+      res.send("Welcome to the playground");
+      next();
+    },
+    (req, res, next) => {
+      console.log("2");
+      res.send("Welcome to the playground");
+      next();
+    },
+  ],
+  (req, res, next) => {
+    res.send("Welcome to the playground");
+    next();
+  },
+  (req, res, next) => {
+    res.send("Welcome to the playground");
+    //next();
+  }
+);
 
 app.listen(7777, () => {
   console.log("Server is running on this port 7777");
