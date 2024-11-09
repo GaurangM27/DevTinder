4d86e87136a55ce85e7b2504849ba7eee5f4081c
GaurangM27 <gaurangmishra2712@gmail.com>
2024-11-10 00:17:36 +0530

    crud api

--- src/app.js
+++ src/app.js
@@ -8,6 +8,30 @@ const connectDb = require("./config/database");
 
 app.use(express.json());
 
+app.get("/user", async (req, res) => {
+  const email = req.body.emailId;
+  try {
+    const user = await User.findOne({ emailId: email });
+
+    if (user.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (error) {
+    res.status(404).send("User not found");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(500).send("Something went wrong");
+  }
+});
+
 app.post("/signup", async (req, res) => {
   const user = new User(req.body);
 
@@ -16,6 +40,26 @@ app.post("/signup", async (req, res) => {
     res.send("User created successfully");
   } catch (error) {
     res.status(400).send("Error while creating user");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const email = req.body.email;
+  try {
+    await User.findOneAndDelete({ emailId: email });
+    res.send("User deleted successfully");
+  } catch (error) {
+    res.status(404).send("User not found");
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const email = req.body.email;
+  try {
+    await User.findOneAndUpdate({ emailId: email }, { password: "meisGM2712" });
+    res.send("User updated successfully");
+  } catch (error) {
+    res.status(404).send("User not found");
   }
 });
 
