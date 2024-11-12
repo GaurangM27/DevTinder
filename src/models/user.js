4559868cf668eae44b719c0c39aae65ed424c9d7
GaurangM27 <gaurangmishra2712@gmail.com>
2024-11-12 12:39:58 +0530

    apply schema validation

--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,56 @@
 const mongoose = require("mongoose");
 
-const userModel = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userModel = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 50,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 20,
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value.toLowerCase())) {
+          throw new Error("Invalid gender value");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg?t=st=1731388831~exp=1731392431~hmac=edb5fa8fc455654e037f638b99e098f44ee394eed239e53fe4af97ef29bf580b&w=740",
+    },
+    about: {
+      type: String,
+      default: "This is the about section",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userModel);
 
