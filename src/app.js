1a6484169e97ee1f814b38ac405dce3ff9aaaef3
GaurangM27 <gaurangmishra2712@gmail.com>
2025-01-15 01:23:20 +0530

    chore: Update CORS origin to use production URL

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://devtinder-frontend-d1wg.onrender.com",
     credentials: true,
   })
 );
