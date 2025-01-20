f7ef36a43e4ae64017573589c60b32224ad88270
GaurangM27 <gaurangmishra2712@gmail.com>
2025-01-20 17:39:47 +0530

    chore: Update MongoDB connection URI

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connectDb = async () => {
-  await mongoose.connect(process.env.URI);
+  await mongoose.connect(
+    "mongodb+srv://gaurangmishra2712:meisGM2712@cluster0.9xohvjb.mongodb.net/devTinder"
+  );
 };
 
 module.exports = connectDb;
